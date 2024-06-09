85ce0c56479f6fcc3ceee66c7200c072c37ecaee
tuanFMV2276 <tuanbqqe170037@fpt.edu.vn>
2024-06-09 13:56:23 +0700

    update api link

--- Modules/Admin/Repositories/BaseRepository/BaseRepository.php
+++ Modules/Admin/Repositories/BaseRepository/BaseRepository.php
@@ -5,28 +5,26 @@ use Illuminate\Support\Facades\Http;
 
 class BaseRepository implements BaseRepositoryInterface
 {
-    
-    
+    private $customer, $diamondShell, $employee, $diamondPriceList, $exDiamond, $mainDiamond, 
+    $order, $orderDetail, $payment, $product, $rewardPoint, $role, $warrantyCetificate;
 
     // Constructor
     public function __construct() {
-        $customer = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $diamondShell = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $employee = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $diamondPriceList = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $exDiamond = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $mainDiamond =Http::get('http://127.0.0.1:8000/api/author')->json();
-        $order = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $orderDetail = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $payment = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $product = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $rewardPoint = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $role = Http::get('http://127.0.0.1:8000/api/author')->json();
-        $warrantyCetificate = Http::get('http://127.0.0.1:8000/api/author')->json();
+        $customer = Http::get('http://127.0.0.1:8000/api/customer')->json();
+        $diamondShell = Http::get('http://127.0.0.1:8000/api/diamondShell')->json();
+        $employee = Http::get('http://127.0.0.1:8000/api/employee')->json();
+        $diamondPriceList = Http::get('http://127.0.0.1:8000/api/diamondpricelist')->json();
+        $exDiamond = Http::get('http://127.0.0.1:8000/api/exdiamond')->json();
+        $mainDiamond =Http::get('http://127.0.0.1:8000/api/maindiamond')->json();
+        $order = Http::get('http://127.0.0.1:8000/api/order')->json();
+        $orderDetail = Http::get('http://127.0.0.1:8000/api/orderdetail')->json();
+        $payment = Http::get('http://127.0.0.1:8000/api/payment')->json();
+        $product = Http::get('http://127.0.0.1:8000/api/product')->json();
+        $rewardPoint = Http::get('http://127.0.0.1:8000/api/rewardpoint')->json();
+        $role = Http::get('http://127.0.0.1:8000/api/role')->json();
+        $warrantyCetificate = Http::get('http://127.0.0.1:8000/api/warrantycertificate')->json();
     }
 
-    private $customer, $diamondShell, $employee, $diamondPriceList, $exDiamond, $mainDiamond, 
-    $order, $orderDetail, $payment, $product, $rewardPoint, $role, $warrantyCetificate;
 
     public function getCustomer() {
         return $this->customer;
