c91038f852317935675c27501bd4d8b9910b0b4b
Kochiya123 <quanhvse184756@fpt.edu.vn>
2024-06-23 13:36:27 +0700

    update dashboard

--- resources/views/dashboardreplica.blade.php
+++ resources/views/dashboardreplica.blade.php
@@ -38,8 +38,6 @@ $dataPointsPieShell = array(
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    <!-- Favicon -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
 </head>
 <body>
     <!-- Top section -->
